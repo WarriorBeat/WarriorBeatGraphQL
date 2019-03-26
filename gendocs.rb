f55abd9182fc4ec9aa213208ad35359347d0ee9a
Braden Mars <BradenM@users.noreply.github.com>
2019-03-26 01:05:48 -0500

    fix(docs): Fixed Docs not loading css

--- gendocs.rb
+++ gendocs.rb
@@ -1,5 +1,5 @@
 require 'graphql-docs'
 
 puts("Generating Docs...")
-GraphQLDocs.build(filename: "schema.graphql", delete_output:true, output_dir: "./docs")
+GraphQLDocs.build(filename: "schema.graphql", delete_output:true, output_dir: "./docs", base_url:"/WarriorBeatGraphQL")
 puts("Docs Generated")
