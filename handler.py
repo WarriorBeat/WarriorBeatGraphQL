3a0cdbab027b6474dce96c60d0e1797c2839c6f2
Braden Mars <BradenM@users.noreply.github.com>
2019-03-27 23:51:20 -0500

    feat(lambda): Added mediaCreate, mediaDelete Lambda Resolvers

--- handler.py
+++ handler.py
@@ -1,29 +1,68 @@
+"""
+    WarriorBeatGraphQL
+    GraphQL Resolvers
+"""
+
 try:
     import unzip_requirements
 except ImportError:
     pass
 import json
-import boto3
-from boto3.dynamodb.conditions import Key
 import uuid
-import datetime
+import os
+from datetime import datetime
+from data import DynamoDB, S3Storage
 
-TABLES = {
-    'polls': {
-        'table_name': 'Polls',
-        'primary_key': 'id'
-    },
-    'poll_options': {
-        'table_name': 'PollOptions',
-        'primary_key': 'id'
-    },
-    'poll_votes': {
-        'table_name': 'PollVotes',
-        'primary_key': 'id'
-    },
+
+def handle_media_delete(*args, **kwargs):
+    """
+    Deletes file from S3 and DynamoDB
+    Returns Media
+    """
+    media_table = DynamoDB('media')
+    media_s3 = S3Storage('media')
+    media_id = kwargs.get("id")
+    media_obj = media_table.get_item(media_id)
+    media_s3_key = media_obj['key']
+
+    media_s3.delete(media_s3_key)
+    media_table.delete_item(media_id)
+    return kwargs
+
+
+def handle_media_create(*args, **kwargs):
+    """
+    Creates file in S3 from source
+    Create media item entry in database
+    returns Media
+    """
+    media_table = DynamoDB('media')
+    media_s3 = S3Storage('media')
+    create_date = str(datetime.now().isoformat())
+    input_args = kwargs.get("media")
+    media = {
+        'id': str(uuid.uuid4()),
+        'authorId': input_args.get('authorId'),
+        'source': input_args.get('source'),
+        'credits': input_args.get('credits', ''),
+        'caption': input_args.get('caption', ''),
+        'createdOn': create_date,
+        'lastUpdated': create_date,
+        'key': '',
+        'url': ''
+    }
+    key = f"{media['authorId']}_{media['id']}"
+    media['url'], _ = media_s3.upload_from_url(media['source'], key=key)
+    media['key'] = key
+    media_table.add_item(media)
+    print(media)
+    return media
+
+
+resolvers = {
+    'mediaCreate': handle_media_create,
+    'mediaDelete': handle_media_delete
 }
-
-dynamodb = boto3.resource("dynamodb")
 
 
 def lambda_handler(event, context):
@@ -32,6 +71,8 @@ def lambda_handler(event, context):
     print(f"Context: {context.__dict__}")
 
     field = event['field']
-    args = event['arguments']
+    args = event['args']
+    print("Environment:", os.environ)
 
-    return {}
+    resolve = resolvers[field]
+    return resolve(**args)
