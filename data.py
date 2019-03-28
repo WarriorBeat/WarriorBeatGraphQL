2bd9420e54db0c282b0a7959af045451b8a3a2da
Braden Mars <BradenM@users.noreply.github.com>
2019-03-27 23:50:38 -0500

    feat(lambda): Added Data Module Utility for Lambda Resolvers

---
+++ data.py
@@ -0,0 +1,151 @@
+"""
+    WarriorBeatGraphQL
+    Data Handlers
+"""
+
+
+import os
+
+import boto3
+import requests
+from botocore.exceptions import ClientError
+
+
+# Connection Info
+TABLES = {
+    'media': {
+        'table_name': os.environ.get("DB_MEDIA"),
+    },
+}
+
+BUCKETS = {
+    'media': {
+        'bucket_name': os.environ.get("S3_MEDIA"),
+        'parent_key': 'media/'
+    }
+}
+
+
+class DynamoDB:
+    """
+    AWS Boto3 DynamoDB
+    Handles transactions with database
+
+    params:
+    table_name: string
+        name of table to access
+    """
+
+    def __init__(self, table):
+        self.dynamodb = boto3.resource('dynamodb')
+        self.table = TABLES[table]
+        self.hash = self.table.get('primary_key', 'id')
+        self.db = self.dynamodb.Table(self.table['table_name'])
+
+    def clean_item(self, item):
+        """prepare item for database insertion"""
+        clean = item.copy()
+        for k, v in item.items():
+            if type(v) is str and len(v) <= 0:
+                clean.pop(k)
+        return clean
+
+    def add_item(self, item):
+        """adds item to database"""
+        item = self.clean_item(item)
+        self.db.put_item(Item=item)
+        return item
+
+    def delete_item(self, itemId):
+        """deletes item from database"""
+        self.db.delete_item(Key={
+            self.hash: itemId
+        })
+        return itemId
+
+    def get_item(self, itemId):
+        """retrieves an item from database"""
+        try:
+            resp = self.db.get_item(Key={
+                self.hash: itemId
+            })
+            return resp.get('Item')
+        except ClientError as e:
+            print(e)
+            return None
+
+    def exists(self, itemId):
+        """checks if an item exists in the database"""
+        item = self.get_item(itemId)
+        return False if item is None else item
+
+    @property
+    def all(self):
+        """list all items in the database table"""
+        resp = self.db.scan()
+        items = resp["Items"]
+        return items
+
+
+class S3Storage:
+    """
+    AWS S3 Bucket
+    Handles transactions with s3 Buckets
+
+    params:
+    bucket_name: string
+        name of bucket to access
+    """
+
+    FILE_EXTENSIONS = {
+        "image/jpeg": ".jpeg",
+        "image/png": ".png"
+    }
+
+    def __init__(self, bucket):
+        self.s3bucket = boto3.resource('s3')
+        self.bucket = BUCKETS[bucket]
+        self.storage = self.s3bucket.Bucket(self.bucket['bucket_name'])
+        self.key = self.bucket['parent_key']
+
+    def get_url(self, key, **kwargs):
+        """generates url where the image is hosted"""
+        aws_root = "https://s3.amazonaws.com"
+        url = f"{aws_root}/{self.bucket['bucket_name']}/{key}"
+        return url
+
+    def upload(self, path, key=None):
+        """uploads a file to the s3 bucket"""
+        if key:
+            key = self.key + key
+            self.storage.upload_file(path, key, ACL='public-read')
+            return self.get_url(key)
+        else:
+            self.storage.upload_file(path, self.key, ACL='public-read')
+            return self.get_url(self.key)
+
+    def upload_obj(self, obj, key=''):
+        """upload file object"""
+        _key = self.key + key + obj[1]
+        self.storage.put_object(Key=_key, Body=obj[0], ACL='public-read')
+        return self.get_url(_key)
+
+    def upload_from_url(self, url, **kwargs):
+        """upload file from url"""
+        img_stream = requests.get(url, stream=True)
+        content_type = img_stream.headers.get('content-type', 'image/jpeg')
+        file_ext = self.FILE_EXTENSIONS[content_type]
+        img_obj = img_stream.raw
+        img_data = img_obj.read()
+        return (self.upload_obj((img_data, file_ext), **kwargs), file_ext)
+
+    def delete(self, keys):
+        """deletes files from s3 bucket"""
+        key_objs = [{'Key': k} for k in keys] if type(keys) is list else [
+            {'Key': keys}]
+        self.storage.delete_objects(
+            Delete={
+                'Objects': key_objs,
+            },
+        )
+        return key_objs
